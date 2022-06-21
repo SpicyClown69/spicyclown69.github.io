004d5478a67cca588329e3e14d4cec6d54a78a5e
SpicyClown69 <101468777+SpicyClown69@users.noreply.github.com>
2022-06-21 20:57:06 +0100

    imrpoved ip gen

--- IP Address Generator/script.js
+++ IP Address Generator/script.js
@@ -7,3 +7,25 @@ function ipGen() {
         display.innerHTML = display.innerHTML + "<br>" + ip
     }
 }
+
+function download() {
+    if (document.getElementById("display").innerText == "No Address Generated") {
+        alert("No Addresses Generated")
+        return
+    }
+    console.log()
+    let filename = "ip-" +  Math.max(1,document.getElementById("amount").value).toString();
+    var element = document.createElement('a');
+    element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(document.getElementById("display").innerText));
+    element.setAttribute('download', filename);
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+function copy() {
+    if (document.getElementById("display").innerText == "No Address Generated") {
+        alert("No Addresses Generated")
+        return
+    }
+    navigator.clipboard.writeText(document.getElementById("display").innerText);
+}
